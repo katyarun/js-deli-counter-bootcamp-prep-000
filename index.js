445f8f915c580a4059386e829fefee99f1b05f52
katyarun@gmail.com <katyarun@gmail.com>
2018-05-16 02:49:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   console.log(katzDeliLine);
   console.log(katzDeliLine.length);
-  return `"Welcome, Ada. You are number ${katzDeliLine.length} in line."`;
+  return `"Welcome, ${name}. You are number ${katzDeliLine.length} in line."`;
 }
 
 const katzDeliLine = ["name1", 'name2'];
